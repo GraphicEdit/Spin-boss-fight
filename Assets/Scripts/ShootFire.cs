b6b2c1b6dde763c869e58077223f0a412555e11b
GraphicEdit <contact@graphicedit.com>
2025-01-23 20:45:54 +0000

    VFX

--- Assets/Scripts/ShootFire.cs
+++ Assets/Scripts/ShootFire.cs
@@ -3,13 +3,19 @@ using UnityEngine;
 public class ShootFire : MonoBehaviour
 {
     public Camera Cam;
+    public GameObject VFX;
     public HealthComponent healthComponent;
+    public AudioSource audioSource;
 
     void Update()
     {
+
         if (Input.GetMouseButtonDown(1) && Cam != null)
         {
             FireCast();
+            VFX.SetActive(true);
+            audioSource.Play();
+            Invoke(nameof(VFXstop), 0.2f);
         }
     }
 
@@ -17,17 +23,22 @@ public class ShootFire : MonoBehaviour
     {
         if (Physics.Raycast(Cam.transform.position, Cam.transform.forward, out RaycastHit hit, 100))
         {
-            Debug.Log(hit.transform.name);
-            Debug.DrawRay(Cam.transform.position, Cam.transform.forward * 100, Color.red, 2.0f);
+            //Debug.Log(hit.transform.name);
+            //Debug.DrawRay(Cam.transform.position, Cam.transform.forward * 100, Color.red, 2.0f);
 
             if (hit.transform.CompareTag("Enemy"))
             {
                 healthComponent.ApplyDamage(10f);
             }
 
-
         }
+
     }
 
+
+    void VFXstop()
+    {
+        VFX.SetActive(false);
+    }
 
 }
